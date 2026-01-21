9216e197463bacf485280af9462c9a88d6d81262
Westly Bouchard <westly.bouchard@gmail.com>
2026-01-21 11:28:39 -0700

    Desktop navbar in a decent spot

--- js/includes.js
+++ js/includes.js
@@ -1,8 +1,13 @@
 async function include(selector, url) {
+    console.log("include called");
+    // Find div with selector
     const el = document.querySelector(selector);
     if (!el) return;
 
+    // Fetch content to put there
     const res = await fetch(url);
+
+    // Put the content there
     el.innerHTML = await res.text();
 }
 
